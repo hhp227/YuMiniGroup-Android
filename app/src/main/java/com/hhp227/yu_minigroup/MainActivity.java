2671bb4cbb1e3eea2a435aaff84d5756a4bc3796
hhp227 <hong227@naver.com>
2019-12-05 18:20:48 +0900

    Initialize New Project

---
+++ app/src/main/java/com/hhp227/yu_minigroup/MainActivity.java
@@ -0,0 +1,25 @@
+package com.hhp227.yu_minigroup;
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.tv_test);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "테스트", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
