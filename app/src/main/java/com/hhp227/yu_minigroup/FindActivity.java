df1c2f879c9e38a54092a3c6223f4d012d89d7f5
hhp227 <hong227@naver.com>
2020-01-16 23:40:08 +0900

    2020.1.16 Commit

--- app/src/main/java/com/hhp227/yu_minigroup/FindActivity.java
+++ app/src/main/java/com/hhp227/yu_minigroup/FindActivity.java
@@ -6,7 +6,6 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
-import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.view.View;
