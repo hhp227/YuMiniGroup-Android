ef8cbed79b70b9bcecea213c61ff3229863b8158
hhp227 <hong227@naver.com>
2020-03-13 22:20:30 +0900

    2020.03.13 Commit

---
+++ app/src/main/java/com/hhp227/yu_minigroup/helper/MyYouTubeBaseActivity.java
@@ -0,0 +1,47 @@
+package com.hhp227.yu_minigroup.helper;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatCallback;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.view.ActionMode;
+import androidx.appcompat.widget.Toolbar;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+
+public class MyYouTubeBaseActivity extends YouTubeBaseActivity implements AppCompatCallback {
+    private AppCompatDelegate mDelegate;
+
+    @NonNull
+    public AppCompatDelegate getDelegate() {
+        if (mDelegate == null) {
+            mDelegate = AppCompatDelegate.create(this, this);
+        }
+        return mDelegate;
+    }
+
+    @Nullable
+    public ActionBar getSupportActionBar() {
+        return getDelegate().getSupportActionBar();
+    }
+
+    public void setSupportActionBar(@Nullable Toolbar toolbar) {
+        getDelegate().setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public void onSupportActionModeStarted(ActionMode mode) {
+
+    }
+
+    @Override
+    public void onSupportActionModeFinished(ActionMode mode) {
+
+    }
+
+    @Nullable
+    @Override
+    public ActionMode onWindowStartingSupportActionMode(ActionMode.Callback callback) {
+        return null;
+    }
+}
