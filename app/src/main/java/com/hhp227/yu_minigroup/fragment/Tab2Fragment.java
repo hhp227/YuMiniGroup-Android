5145fb7a71171a080b5453d21cd958497ed0cc2d
hhp227 <hhp227@kortek.co.kr>
2022-03-15 18:51:42 +0900

    2022.03.15 Commit

--- app/src/main/java/com/hhp227/yu_minigroup/fragment/Tab2Fragment.java
+++ app/src/main/java/com/hhp227/yu_minigroup/fragment/Tab2Fragment.java
@@ -17,13 +17,19 @@ import com.hhp227.yu_minigroup.app.AppController;
 import com.hhp227.yu_minigroup.databinding.FragmentTab2Binding;
 import com.hhp227.yu_minigroup.databinding.HeaderCalendarBinding;
 import com.hhp227.yu_minigroup.databinding.ScheduleItemBinding;
-import net.htmlparser.jericho.Element;
-import net.htmlparser.jericho.HTMLElementName;
-import net.htmlparser.jericho.Source;
 
+import java.io.StringReader;
 import java.util.*;
 
 import static com.hhp227.yu_minigroup.app.EndPoint.URL_SCHEDULE;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 public class Tab2Fragment extends Fragment {
     private static final int TYPE_CALENDAR = 0;
@@ -101,12 +107,31 @@ public class Tab2Fragment extends Fragment {
 
         //URL_SCHEDULE.replace("{YEAR}", year)
         AppController.getInstance().addToRequestQueue(new StringRequest(Request.Method.POST, URL_SCHEDULE, response -> {
-            Source source = new Source(response);
+            //Source source = new Source(response);
+            DocumentBuilder documentBuilder;
+            Document document;
 
             try {
+                documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+                document = documentBuilder.parse(new InputSource(new StringReader(response)));
+                Element documentElement = document.getDocumentElement();
+                NodeList elementsByTagName = documentElement.getElementsByTagName("Items");
+
+                if (elementsByTagName != null && elementsByTagName.getLength() > 0) {
+                    for (int i = 0; i < elementsByTagName.getLength(); i++) {
+                        Element element = (Element) elementsByTagName.item(i);
+                        Element element1 = (Element) element.getElementsByTagName("Subject").item(0);
+                        Element element2 = (Element) element.getElementsByTagName("Date").item(0);
+                        Element element3 = (Element) element.getElementsByTagName("Author").item(0);
+                        Element element4 = (Element) element.getElementsByTagName("Text").item(0);
+                        Element element5 = (Element) element.getElementsByTagName("Date").item(0);
+                        Element element6 = (Element) element.getElementsByTagName("Link").item(0);
+
+                        Log.e("TEST", "element1: " + getParsing(element1) + ", element2: " + getParsing(element2) + ", element3: " + getParsing(element3) + ", element4: " + getParsing(element4) + ", element5: " + getParsing(element5) + ", element6: " + getParsing(element6));
+                    }
+                }
                 mList.clear();
                 addHeaderView();
-                Log.e("TEST", "response: " + response);
                 /*Element infoCalendar = source.getFirstElementByClass("info_calendar case");
 
                 for (int i = 0; i < infoCalendar.getAllElements(HTMLElementName.A).size(); i++) {
@@ -119,7 +144,6 @@ public class Tab2Fragment extends Fragment {
                             mList.add(map);
                         });
                 }*/
-                Log.e("TEST", "source: " + source);
                 mAdapter.notifyDataSetChanged();
             } catch (Exception e) {
                 Log.e(TAG, e.getMessage());
@@ -130,149 +154,20 @@ public class Tab2Fragment extends Fragment {
         }));
     }
 
-    /*private void DomParser(String str) {
-        URL url2;
-        HttpURLConnection httpURLConnection;
-        int i;
-        InputStream inputStream;
-        DocumentBuilder documentBuilder;
-        String str2;
-        String str3;
-        String str4;
-        String str5;
-        String str6;
-        String str7;
-        String str8;
-        String str9;
-        String str10;
-        String str11;
-        Document document = null;
-        try {
-            url2 = new URL(str);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-            url2 = null;
-        }
-        try {
-            httpURLConnection = (HttpURLConnection) url2.openConnection();
-        } catch (IOException e2) {
-            e2.printStackTrace();
-            httpURLConnection = null;
-        }
-        if (httpURLConnection != null) {
-            httpURLConnection.setConnectTimeout(SearchAuth.StatusCodes.AUTH_DISABLED);
-            httpURLConnection.setUseCaches(false);
-            try {
-                i = httpURLConnection.getResponseCode();
-            } catch (IOException e3) {
-                e3.printStackTrace();
-                i = 0;
-            }
-            if (i == 200) {
-                try {
-                    inputStream = httpURLConnection.getInputStream();
-                } catch (IOException e4) {
-                    e4.printStackTrace();
-                    inputStream = null;
-                }
-                try {
-                    documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-                } catch (ParserConfigurationException e5) {
-                    e5.printStackTrace();
-                    documentBuilder = null;
-                }
-                try {
-                    document = documentBuilder.parse(inputStream);
-                } catch (SAXException e6) {
-                    e6.printStackTrace();
-                } catch (IOException e7) {
-                    e7.printStackTrace();
-                }
-                Element documentElement = document.getDocumentElement();
-                aryGeneral.clear();
-                this.aryItem.clear();
-                NodeList elementsByTagName = documentElement.getElementsByTagName("Items");
-                if (elementsByTagName != null && elementsByTagName.getLength() > 0) {
-                    for (int i2 = 0; i2 < elementsByTagName.getLength(); i2++) {
-                        Element element = (Element) elementsByTagName.item(i2);
-                        Element element2 = (Element) element.getElementsByTagName("Subject").item(0);
-                        Element element3 = (Element) element.getElementsByTagName("Date").item(0);
-                        Element element4 = (Element) element.getElementsByTagName("Author").item(0);
-                        Element element5 = (Element) element.getElementsByTagName("Text").item(0);
-                        Element element6 = (Element) element.getElementsByTagName("Date").item(0);
-                        Element element7 = (Element) element.getElementsByTagName("Link").item(0);
-                        String str12 = "";
-                        if (element2 != null) {
-                            try {
-                                str11 = element2.getFirstChild().getNodeValue();
-                            } catch (Exception unused) {
-                                str2 = str12;
-                            }
-                        } else {
-                            str11 = str12;
-                        }
-                        str2 = str11;
-                        if (element3 != null) {
-                            try {
-                                str10 = element3.getFirstChild().getNodeValue();
-                            } catch (Exception unused2) {
-                                str3 = str12;
-                            }
-                        } else {
-                            str10 = str12;
-                        }
-                        str3 = str10;
-                        if (element4 != null) {
-                            try {
-                                str9 = element4.getFirstChild().getNodeValue();
-                            } catch (Exception unused3) {
-                                str4 = str12;
-                            }
-                        } else {
-                            str9 = str12;
-                        }
-                        str4 = str9;
-                        if (element5 != null) {
-                            try {
-                                str8 = element5.getFirstChild().getNodeValue();
-                            } catch (Exception unused4) {
-                                str5 = str12;
-                            }
-                        } else {
-                            str8 = str12;
-                        }
-                        str5 = str8;
-                        if (element6 != null) {
-                            try {
-                                str7 = element6.getFirstChild().getNodeValue();
-                            } catch (Exception unused5) {
-                                str6 = str12;
-                            }
-                        } else {
-                            str7 = str12;
-                        }
-                        str6 = str7;
-                        if (element7 != null) {
-                            try {
-                                str12 = element7.getFirstChild().getNodeValue();
-                            } catch (Exception unused6) {
-                            }
-                        }
-                        aryGeneral.add(new FoodItem(str2, str3, str4, str5, str6, str12));
-                    }
-                }
-            }
-        }
-        httpURLConnection.disconnect();
-        for (int i3 = 0; i3 < aryGeneral.size(); i3++) {
-            FoodItem foodItem = aryGeneral.get(i3);
-            this.aryItem.add(new EduListItem(foodItem.getRecn(), foodItem.getStitle(), foodItem.getAuthor(), foodItem.getPublisher(), foodItem.getPubyear(), foodItem.getCallnumber()));
-        }
-    }*/
-
-
     public void addHeaderView() {
         mList.add(new HashMap<>());
+    }
+
+    private String getParsing(Element element) {
+        if (element != null) {
+            try {
+                return element.getFirstChild().getNodeValue();
+            } catch (Exception unused2) {
+                return "error";
+            }
+        } else {
+            return "error";
+        }
     }
 
     public class HeaderHolder extends RecyclerView.ViewHolder {
