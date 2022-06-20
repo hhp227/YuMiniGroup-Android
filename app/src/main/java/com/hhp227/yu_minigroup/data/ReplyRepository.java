89d1a288f5f0a12b6c93db6b4c3c78aa042d0c21
hhp227 <hhp227@kortek.co.kr>
2022-06-20 10:33:12 +0900

    2022.06.20 Commit

--- app/src/main/java/com/hhp227/yu_minigroup/data/ReplyRepository.java
+++ app/src/main/java/com/hhp227/yu_minigroup/data/ReplyRepository.java
@@ -1,7 +1,202 @@
 package com.hhp227.yu_minigroup.data;
 
-public class ReplyRepository {
-    public void getReplyList() {
+import android.text.Html;
 
+import androidx.annotation.NonNull;
+
+import com.android.volley.Request;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.StringRequest;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.hhp227.yu_minigroup.app.AppController;
+import com.hhp227.yu_minigroup.app.EndPoint;
+import com.hhp227.yu_minigroup.dto.ReplyItem;
+import com.hhp227.yu_minigroup.dto.User;
+import com.hhp227.yu_minigroup.helper.Callback;
+
+import net.htmlparser.jericho.Element;
+import net.htmlparser.jericho.HTMLElementName;
+import net.htmlparser.jericho.Source;
+
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ReplyRepository {
+    private final String mGroupId, mArticleId, mArticleKey;
+
+    public ReplyRepository(String groupId, String articleId, String articleKey) {
+        this.mGroupId = groupId;
+        this.mArticleId = articleId;
+        this.mArticleKey = articleKey;
+    }
+
+    public void getReplyList(List<Element> commentList, Callback callback) {
+        List<Map.Entry<String, ReplyItem>> replyItemList = new ArrayList<>();
+
+        try {
+            for (Element comment : commentList) {
+                Element commentName = comment.getFirstElementByClass("comment-name");
+                Element commentAddr = comment.getFirstElementByClass("comment-addr");
+                String replyId = commentAddr.getAttributeValue("id").replace("cmt_txt_", "");
+                String name = commentName.getTextExtractor().toString().trim();
+                String timeStamp = commentName.getFirstElement(HTMLElementName.SPAN).getContent().toString().trim();
+                String replyContent = commentAddr.getContent().toString().trim();
+                boolean authorization = commentName.getAllElements(HTMLElementName.INPUT).size() > 0;
+                ReplyItem replyItem = new ReplyItem();
+
+                replyItem.setId(replyId);
+                replyItem.setName(name.substring(0, name.lastIndexOf("(")));
+                replyItem.setReply(Html.fromHtml(replyContent).toString());
+                replyItem.setDate(timeStamp.replaceAll("[(]|[)]", ""));
+                replyItem.setAuth(authorization);
+                replyItemList.add(new AbstractMap.SimpleEntry<>(replyId, replyItem));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            callback.onFailure(e);
+        } finally {
+            fetchReplyListFromFirebase(replyItemList, callback);
+        }
+    }
+
+    public void addReply(String cookie, User user, String text, Callback callback) {
+        String tag_string_req = "req_send";
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, EndPoint.INSERT_REPLY, response -> {
+            Source source = new Source(response);
+            List<Element> commentList = source.getAllElementsByClass("comment-list");
+
+            try {
+                insertReplyToFirebase(commentList, text, user, callback);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }, error -> {
+            VolleyLog.e(error.getMessage());
+            callback.onFailure(error);
+        }) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+
+                headers.put("Cookie", cookie);
+                return headers;
+            }
+
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+
+                params.put("CLUB_GRP_ID", mGroupId);
+                params.put("ARTL_NUM", mArticleId);
+                params.put("CMT", text);
+                return params;
+            }
+        };
+
+        AppController.getInstance().addToRequestQueue(stringRequest, tag_string_req);
+    }
+
+    public void removeReply(String cookie, String replyId, String replyKey, Callback callback) {
+        String tag_string_req = "req_delete";
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, EndPoint.DELETE_REPLY, response -> {
+            Source source = new Source(response);
+
+            try {
+                if (!response.contains("처리를 실패했습니다")) {
+                    List<Element> commentList = source.getAllElementsByClass("comment-list");
+
+                    deleteReplyFromFirebase(replyKey, commentList, callback);
+                }
+            } catch (Exception e) {
+                callback.onFailure(e);
+                e.printStackTrace();
+            }
+        }, error -> {
+            VolleyLog.e(error.getMessage());
+            callback.onFailure(error);
+        }) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+
+                headers.put("Cookie", cookie);
+                return headers;
+            }
+
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+
+                params.put("CLUB_GRP_ID", mGroupId);
+                params.put("CMMT_NUM", replyId);
+                params.put("ARTL_NUM", mArticleId);
+                return params;
+            }
+        };
+
+        callback.onLoading();
+        AppController.getInstance().addToRequestQueue(stringRequest, tag_string_req);
+
+    }
+
+    private void fetchReplyListFromFirebase(List<Map.Entry<String, ReplyItem>> replyItemList, Callback callback) {
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Replys");
+
+        databaseReference.child(mArticleKey).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    String key = snapshot.getKey();
+                    ReplyItem value = snapshot.getValue(ReplyItem.class);
+
+                    if (value != null) {
+                        int index = replyItemList.stream().map(Map.Entry::getKey).collect(Collectors.toList()).indexOf(value.getId());
+
+                        if (index > -1) {
+                            Map.Entry<String, ReplyItem> entry = replyItemList.get(index);
+                            ReplyItem replyItem = entry.getValue();
+
+                            replyItem.setUid(value.getUid());
+                            replyItemList.set(index, new AbstractMap.SimpleEntry<>(key, replyItem));
+                        }
+                    }
+                }
+                callback.onSuccess(replyItemList);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                callback.onFailure(databaseError.toException());
+            }
+        });
+    }
+
+    private void insertReplyToFirebase(List<Element> commentList, String text, User user, Callback callback) {
+        String replyId = commentList.get(commentList.size() - 1).getFirstElementByClass("comment-addr").getAttributeValue("id").replace("cmt_txt_", "");
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Replys");
+        ReplyItem replyItem = new ReplyItem();
+
+        replyItem.setId(replyId);
+        replyItem.setUid(user.getUid());
+        replyItem.setName(user.getName());
+        replyItem.setTimestamp(System.currentTimeMillis());
+        replyItem.setReply(text);
+        databaseReference.child(mArticleKey).push().setValue(replyItem);
+        callback.onSuccess(commentList);
+    }
+
+    private void deleteReplyFromFirebase(String replyKey, List<Element> commentList, Callback callback) {
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Replys");
+
+        databaseReference.child(mArticleKey).child(replyKey).removeValue();
+        callback.onSuccess(commentList);
     }
 }
--- app/src/main/java/com/hhp227/yu_minigroup/viewmodel/ArticleViewModel.java
+++ app/src/main/java/com/hhp227/yu_minigroup/viewmodel/ArticleViewModel.java
@@ -1,45 +1,27 @@
 package com.hhp227.yu_minigroup.viewmodel;
 
-import static java.util.Objects.requireNonNull;
-
 import android.os.Parcel;
 import android.os.Parcelable;
-import android.text.Html;
-import android.util.Log;
 import android.webkit.CookieManager;
 
-import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.SavedStateHandle;
 import androidx.lifecycle.ViewModel;
 
-import com.android.volley.Request;
-import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.StringRequest;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.hhp227.yu_minigroup.app.AppController;
 import com.hhp227.yu_minigroup.app.EndPoint;
 import com.hhp227.yu_minigroup.data.ArticleRepository;
+import com.hhp227.yu_minigroup.data.ReplyRepository;
 import com.hhp227.yu_minigroup.dto.ArticleItem;
 import com.hhp227.yu_minigroup.dto.ReplyItem;
 import com.hhp227.yu_minigroup.helper.Callback;
 import com.hhp227.yu_minigroup.helper.PreferenceManager;
 
 import net.htmlparser.jericho.Element;
-import net.htmlparser.jericho.HTMLElementName;
-import net.htmlparser.jericho.Source;
 
-import java.util.AbstractMap;
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class ArticleViewModel extends ViewModel {
     public final Boolean mIsAuthorized;
@@ -48,7 +30,7 @@ public class ArticleViewModel extends ViewModel {
 
     public final String mGroupId, mArticleId, mGroupName, mGroupImage, mGroupKey, mArticleKey;
 
-    private static final String TAG = ArticleViewModel.class.getSimpleName(), STATE = "state", REPLY_FORM_STATE = "replyFormState", UPDATE_ARTICLE_STATE = "updateArticleState", ARTICLE = "article";
+    private static final String STATE = "state", REPLY_FORM_STATE = "replyFormState", UPDATE_ARTICLE_STATE = "updateArticleState", ARTICLE = "article";
 
     private final CookieManager mCookieManager = AppController.getInstance().getCookieManager();
 
@@ -56,7 +38,9 @@ public class ArticleViewModel extends ViewModel {
 
     private final SavedStateHandle mSavedStateHandle;
 
-    private final ArticleRepository articleRepository;
+    private final ArticleRepository mArticleRepository;
+
+    private final ReplyRepository mReplyRepository;
 
     public ArticleViewModel(SavedStateHandle savedStateHandle) {
         mSavedStateHandle = savedStateHandle;
@@ -68,7 +52,8 @@ public class ArticleViewModel extends ViewModel {
         mArticleKey = savedStateHandle.get("artl_key");
         mPosition = savedStateHandle.get("position");
         mIsAuthorized = savedStateHandle.get("auth");
-        articleRepository = new ArticleRepository(mGroupId, mGroupKey);
+        mArticleRepository = new ArticleRepository(mGroupId, mGroupKey);
+        mReplyRepository = new ReplyRepository(mGroupId, mArticleId, mArticleKey);
 
         if (!mSavedStateHandle.contains(STATE)) {
             mSavedStateHandle.set(STATE, new State(false, null, Collections.emptyList(), false, null));
@@ -119,53 +104,36 @@ public class ArticleViewModel extends ViewModel {
 
     public void actionSend(String text) {
         if (text.length() > 0) {
-            String tag_string_req = "req_send";
-            StringRequest stringRequest = new StringRequest(Request.Method.POST, EndPoint.INSERT_REPLY, response -> {
-                Source source = new Source(response);
-                List<Element> commentList = source.getAllElementsByClass("comment-list");
-
-                try {
-                    refreshReply(commentList);
+            mReplyRepository.addReply(getCookie(), mPreferenceManager.getUser(), text, new Callback() {
+                @Override
+                public <T> void onSuccess(T data) {
+                    refreshReply((List<Element>) data);
 
                     // 전송할때마다 리스트뷰 아래로
                     setScrollToLastState(true);
-                } catch (Exception e) {
-                    Log.e(TAG, e.getMessage());
-                } finally {
-                    insertReplyToFirebase(commentList.get(commentList.size() - 1).getFirstElementByClass("comment-addr").getAttributeValue("id").replace("cmt_txt_", ""), text);
-                }
-            }, error -> {
-                VolleyLog.e(TAG, error.getMessage());
-                mSavedStateHandle.set(STATE, new State(false, null, Collections.emptyList(), false, error.getMessage()));
-            }) {
-                @Override
-                public Map<String, String> getHeaders() {
-                    Map<String, String> headers = new HashMap<>();
-
-                    headers.put("Cookie", getCookie());
-                    return headers;
                 }
 
                 @Override
-                protected Map<String, String> getParams() {
-                    Map<String, String> params = new HashMap<>();
-
-                    params.put("CLUB_GRP_ID", mGroupId);
-                    params.put("ARTL_NUM", mArticleId);
-                    params.put("CMT", text);
-                    return params;
+                public void onFailure(Throwable throwable) {
+                    setState(new State(false, null, Collections.emptyList(), false, throwable.getMessage()));
                 }
-            };
 
-            mSavedStateHandle.set(STATE, new State(true, ((State) requireNonNull(mSavedStateHandle.get(STATE))).articleItem, Collections.emptyList(), false, null));
-            AppController.getInstance().addToRequestQueue(stringRequest, tag_string_req);
+                @Override
+                public void onLoading() {
+                    State state = mSavedStateHandle.get(STATE);
+
+                    if (state != null) {
+                        setState(new State(true, state.articleItem, Collections.emptyList(), false, null));
+                    }
+                }
+            });
         } else {
             mSavedStateHandle.set(REPLY_FORM_STATE, new ReplyFormState("댓글을 입력하세요."));
         }
     }
 
     public void deleteArticle() {
-        articleRepository.removeArticle(getCookie(), mArticleId, mArticleKey, new Callback() {
+        mArticleRepository.removeArticle(getCookie(), mArticleId, mArticleKey, new Callback() {
             @Override
             public <T> void onSuccess(T data) {
                 setState(new State(false, null, Collections.emptyList(), true, "삭제완료"));
@@ -184,47 +152,26 @@ public class ArticleViewModel extends ViewModel {
     }
 
     public void deleteReply(String replyId, String replyKey) {
-        String tag_string_req = "req_delete";
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, EndPoint.DELETE_REPLY, response -> {
-            Source source = new Source(response);
+        mReplyRepository.removeReply(getCookie(), replyId, replyKey, new Callback() {
+            @Override
+            public <T> void onSuccess(T data) {
+                refreshReply((List<Element>) data);
+            }
 
-            try {
-                if (!response.contains("처리를 실패했습니다")) {
-                    List<Element> commentList = source.getAllElementsByClass("comment-list");
+            @Override
+            public void onFailure(Throwable throwable) {
+                setState(new State(false, null, Collections.emptyList(), false, throwable.getMessage()));
+            }
 
-                    refreshReply(commentList);
+            @Override
+            public void onLoading() {
+                State state = mSavedStateHandle.get(STATE);
+
+                if (state != null) {
+                    setState(new State(true, state.articleItem, state.replyItemList, false, null));
                 }
-            } catch (Exception e) {
-                mSavedStateHandle.set(STATE, new State(false, null, Collections.emptyList(), false, e.getMessage()));
-                Log.e(TAG, e.getMessage());
-            } finally {
-                deleteReplyFromFirebase(replyKey);
             }
-        }, error -> {
-            VolleyLog.e(TAG, error.getMessage());
-            mSavedStateHandle.set(STATE, new State(false, null, Collections.emptyList(), false, error.getMessage()));
-        }) {
-            @Override
-            public Map<String, String> getHeaders() {
-                Map<String, String> headers = new HashMap<>();
-
-                headers.put("Cookie", getCookie());
-                return headers;
-            }
-
-            @Override
-            protected Map<String, String> getParams() {
-                Map<String, String> params = new HashMap<>();
-
-                params.put("CLUB_GRP_ID", mGroupId);
-                params.put("CMMT_NUM", replyId);
-                params.put("ARTL_NUM", mArticleId);
-                return params;
-            }
-        };
-
-        mSavedStateHandle.set(STATE, new State(true, ((State) requireNonNull(mSavedStateHandle.get(STATE))).articleItem, ((State) requireNonNull(mSavedStateHandle.get(STATE))).replyItemList, false, null));
-        AppController.getInstance().addToRequestQueue(stringRequest, tag_string_req);
+        });
     }
 
     public void refresh() {
@@ -238,7 +185,7 @@ public class ArticleViewModel extends ViewModel {
     private void fetchArticleData(String articleId) {
         String params = "?CLUB_GRP_ID=" + mGroupId + "&startL=" + mPosition + "&displayL=1";
 
-        articleRepository.getArticleData(getCookie(), articleId, mArticleKey, params, new Callback() {
+        mArticleRepository.getArticleData(getCookie(), articleId, mArticleKey, params, new Callback() {
             @Override
             public <T> void onSuccess(T data) {
                 State state = mSavedStateHandle.get(STATE);
@@ -271,83 +218,21 @@ public class ArticleViewModel extends ViewModel {
     }
 
     private void fetchReplyData(List<Element> commentList) {
-        List<Map.Entry<String, ReplyItem>> replyItemList = new ArrayList<>();
-
-        try {
-            for (Element comment : commentList) {
-                Element commentName = comment.getFirstElementByClass("comment-name");
-                Element commentAddr = comment.getFirstElementByClass("comment-addr");
-                String replyId = commentAddr.getAttributeValue("id").replace("cmt_txt_", "");
-                String name = commentName.getTextExtractor().toString().trim();
-                String timeStamp = commentName.getFirstElement(HTMLElementName.SPAN).getContent().toString().trim();
-                String replyContent = commentAddr.getContent().toString().trim();
-                boolean authorization = commentName.getAllElements(HTMLElementName.INPUT).size() > 0;
-                ReplyItem replyItem = new ReplyItem();
-
-                replyItem.setId(replyId);
-                replyItem.setName(name.substring(0, name.lastIndexOf("(")));
-                replyItem.setReply(Html.fromHtml(replyContent).toString());
-                replyItem.setDate(timeStamp.replaceAll("[(]|[)]", ""));
-                replyItem.setAuth(authorization);
-                replyItemList.add(new AbstractMap.SimpleEntry<>(replyId, replyItem));
-            }
-        } catch (Exception e) {
-            Log.e(TAG, e.getMessage());
-            mSavedStateHandle.set(STATE, new State(false, null, Collections.emptyList(), false, e.getMessage()));
-        } finally {
-            fetchReplyListFromFirebase(replyItemList);
-        }
-    }
-
-    private void fetchReplyListFromFirebase(List<Map.Entry<String, ReplyItem>> replyItemList) {
-        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Replys");
-
-        databaseReference.child(mArticleKey).addListenerForSingleValueEvent(new ValueEventListener() {
+        mReplyRepository.getReplyList(commentList, new Callback() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    String key = snapshot.getKey();
-                    ReplyItem value = snapshot.getValue(ReplyItem.class);
-
-                    if (value != null) {
-                        int index = replyItemList.stream().map(Map.Entry::getKey).collect(Collectors.toList()).indexOf(value.getId());
-
-                        if (index > -1) {
-                            Map.Entry<String, ReplyItem> entry = replyItemList.get(index);
-                            ReplyItem replyItem = entry.getValue();
-
-                            replyItem.setUid(value.getUid());
-                            replyItemList.set(index, new AbstractMap.SimpleEntry<>(key, replyItem));
-                        }
-                    }
-                }
-                mSavedStateHandle.set(STATE, new State(false, null, replyItemList, false, null));
+            public <T> void onSuccess(T data) {
+                setState(new State(false, null, (List<Map.Entry<String, ReplyItem>>) data, false, null));
             }
 
             @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                Log.e(TAG, "파이어베이스 데이터 불러오기 실패", databaseError.toException());
-                mSavedStateHandle.set(STATE, new State(false, null, Collections.emptyList(), false, databaseError.getMessage()));
+            public void onFailure(Throwable throwable) {
+                setState(new State(false, null, Collections.emptyList(), false, throwable.getMessage()));
+            }
+
+            @Override
+            public void onLoading() {
             }
         });
-    }
-
-    private void insertReplyToFirebase(String replyId, String text) {
-        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Replys");
-        ReplyItem replyItem = new ReplyItem();
-
-        replyItem.setId(replyId);
-        replyItem.setUid(mPreferenceManager.getUser().getUid());
-        replyItem.setName(mPreferenceManager.getUser().getName());
-        replyItem.setTimestamp(System.currentTimeMillis());
-        replyItem.setReply(text);
-        databaseReference.child(mArticleKey).push().setValue(replyItem);
-    }
-
-    private void deleteReplyFromFirebase(String replyKey) {
-        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Replys");
-
-        databaseReference.child(mArticleKey).child(replyKey).removeValue();
     }
 
     public static final class State implements Parcelable {
